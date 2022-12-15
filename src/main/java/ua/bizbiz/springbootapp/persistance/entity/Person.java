ed64408040ac737fcc84c2608c30cb56c371c247
ViktorBizbiz <bizbizgennadij@gmail.com>
2022-12-15 19:44:03 +0200

    Create entity Person

---
+++ src/main/java/ua/bizbiz/springbootapp/persistance/entity/Person.java
@@ -0,0 +1,33 @@
+package ua.bizbiz.springbootapp.persistance.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "person")
+public class Person {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 30, message = "Name should be between 2 and 30 characters")
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "surname")
+    private String surname;
+
+    @Column(name = "birthdate")
+    private String birthdate;
+}
